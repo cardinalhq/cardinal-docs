56800655dfc6fd6de929bb6ec2478a54958e02bd
Kunal Kundaje <kunal@kundaje.net>
2024-09-22 14:27:36 -0700

    Fix logo image, add favicon

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { DocsThemeConfig } from 'nextra-theme-docs';
 
+const basePath = '/cardinal-docs';
+
 const config: DocsThemeConfig = {
   logo: (
     <>
-      <img src="/cardinal.png" width="36" />
+      <img src={`${basePath}/cardinal.png`} width="36" />
       <h1 className="nx-font-bold nx-text-2xl">Cardinal</h1>
+    </>
+  ),
+  head: (
+    <>
+      <link rel="icon" type="image/png" href={`${basePath}/cardinal.png`} />
     </>
   ),
   project: {
