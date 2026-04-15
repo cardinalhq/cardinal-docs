d07ea137b7ac6d2ec60019d96caa2022e7fd1135
ruchirj <ruchir.jha@gmail.com>
2026-04-14 23:02:35 -0700

    Add integrations docs

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,5 +1,6 @@
 export default {
   index: 'Welcome',
+  maestro: 'Cardinal Cloud',
   vibeshield: 'Cardinal Desktop Application',
   lakerunner: 'Lakerunner',
 }
---
+++ content/maestro/integrations/_meta.ts
@@ -0,0 +1,14 @@
+export default {
+  index: 'Overview',
+  lakerunner: 'Lakerunner',
+  postgres: 'PostgreSQL',
+  bigquery: 'Google BigQuery',
+  snowflake: 'Snowflake',
+  athena: 'Amazon Athena',
+  clickhouse: 'ClickHouse',
+  github: 'GitHub',
+  servicenow: 'ServiceNow',
+  slack: 'Slack',
+  teams: 'Microsoft Teams',
+  pagerduty: 'PagerDuty',
+}
